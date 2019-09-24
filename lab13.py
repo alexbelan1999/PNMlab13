200e3b9c4d0d36a7435d1d9ffe776a593ae5604c
alexbelan1999 <alexbelan1999@gmail.com>
2019-09-24 16:13:14 +0300

    Initial commit

---
+++ lab13.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+ITERATION_LIMIT = 1000
+
+A = np.array([[10., -1., 2., 0.],
+              [-1., 11., -1., 3.],
+              [2., -1., 10., -1.],
+              [0.0, 3., -1., 8.]])
+
+b = np.array([6., 25., -11., 15.])
+
+
+print("System:")
+for i in range(A.shape[0]):
+    row = ["{}*x{}".format(A[i, j], j + 1) for j in range(A.shape[1])]
+    print(" + ".join(row), "=", b[i])
+print()
+
+x = np.zeros_like(b)
+for it_count in range(ITERATION_LIMIT):
+    print("Current solution:", x)
+    x_new = np.zeros_like(x)
+
+    for i in range(A.shape[0]):
+        s1 = np.dot(A[i, :i], x[:i])
+        s2 = np.dot(A[i, i + 1:], x[i + 1:])
+        x_new[i] = (b[i] - s1 - s2) / A[i, i]
+
+    if np.allclose(x, x_new, atol=0.001, rtol=0.001):
+        break
+
+    x = x_new
+
+print("Solution:")
+print(x)
+error = np.dot(A, x) - b
+print("Error:")
+print(error)
