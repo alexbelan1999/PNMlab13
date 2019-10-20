2fed5364ee55141e67c290e9ef0c5308d48267a3
alexbelan1999 <alexbelan1999@gmail.com>
2019-10-20 14:30:30 +0300

    Add Jacobi method

--- func.py
+++ func.py
@@ -7,3 +7,35 @@ def diagonal(A):
         if result > A[i][i]:
             diag = False
     return diag
+
+def Jacobi(A,b,E):
+    x = np.zeros_like(b)
+    it_count = 0
+    while (True):
+        print("Итерация ", it_count, " решение на данной итерации:", x)
+        x_new = np.zeros_like(x)
+
+        for i in range(0,A.shape[0]):
+            s1 = np.dot(A[i, :i], x[:i])
+            s2 = np.dot(A[i, i + 1:], x[i + 1:])
+            #print("x_new = " ,b[i]," - ",s1," - ",s2)
+            x_new[i] = (b[i] - s1 - s2) / A[i, i]
+
+        exit = False
+        for n in range(0, A.shape[0]):
+            if abs(x_new[n] - x[n]) <= E:
+                exit = True
+        if exit == True:
+            break
+        x = x_new
+        it_count += 1
+
+    print()
+    print("Решение системы: ")
+    print(x)
+
+    error = np.dot(A, x) - b
+    print()
+    print("Погрешность: ")
+    print(error)
+    pass
--- lab13.py
+++ lab13.py
@@ -1,39 +1,22 @@
 import numpy as np
 import func
-ITERATION_LIMIT = 1000
-
+var = 0
+while var not in [1,2]:
+    var = int(input("Выберете метод 1 - Якоби, 2 - Зейделя: "))
+E = -1
+while E < 0:
+    E = float(input("Введите погрешность: "))
 A = np.array([[3.1, 0.8, 1.9],[1.9, 3.1, 1.1],[0.5, 3.8, 4.8]])
 diag = func.diagonal(A)
 print("Диагональное преобладание: ", diag)
+print()
 
 b = np.array([0.2, 2.1, 5.6])
 
-print("System:")
+print("Система:")
 for i in range(A.shape[0]):
-    row = ["{}*x{}".format(A[i, j], j + 1) for j in range(A.shape[1])]
+    row = ["{} * x{}".format(A[i, j], j + 1) for j in range(A.shape[1])]
     print(" + ".join(row), "=", b[i])
 print()
-
-x = np.zeros_like(b)
-for it_count in range(ITERATION_LIMIT):
-    print("Current solution:", x)
-    x_new = np.zeros_like(x)
-
-    for i in range(A.shape[0]):
-        s1 = np.dot(A[i, :i], x[:i])
-        s2 = np.dot(A[i, i + 1:], x[i + 1:])
-        x_new[i] = (b[i] - s1 - s2) / A[i, i]
-    exit = False
-    for n in range(A.shape[0]):
-        if abs(x_new[n] - x[n])<0.001:
-            exit = True
-    #if np.allclose(x, x_new, atol=0.001, rtol=0.0):
-    if exit == True:
-        break
-    x = x_new
-
-print("Solution:")
-print(x)
-error = np.dot(A, x) - b
-print("Error:")
-print(error)
+if var == 1:
+    func.Jacobi(A,b,E)
