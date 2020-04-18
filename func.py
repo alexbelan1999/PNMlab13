a39ff0224e8916735b8700cbebe9a47f12d1be45
alexbelan1999 <alexbelan1999@gmail.com>
2020-04-18 17:14:59 +0300

    Fix

--- func.py
+++ func.py
@@ -1,19 +1,22 @@
 import numpy as np
 import math
 
+
 def diagonal(A):
     diag = True
-    for i in range(0,len(A)):
+    for i in range(0, len(A)):
         sum1 = sum(A[i])
         result = sum1 - A[i][i]
         if result > A[i][i]:
             diag = False
     return diag
 
-def check(x_new,x,E):
+
+def check(x_new, x, E):
     return math.sqrt(sum((x_new[i] - x[i]) ** 2 for i in range(len(x)))) <= E
 
-def Jacobi(A,b,E):
+
+def Jacobi(A, b, E):
     maxA = 0
     for i in range(0, A.shape[0]):
         if A[i, i] > maxA:
@@ -21,17 +24,17 @@ def Jacobi(A,b,E):
     x = b / maxA
     it_count = 1
     while (True):
-        x_new = b/maxA
-        for i in range(0,A.shape[0]):
-            #print("A[i, :i]: ", A[i, :i])
-            #print("x[:i]: ", x[:i])
+        x_new = b / maxA
+        for i in range(0, A.shape[0]):
+            # print("A[i, :i]: ", A[i, :i])
+            # print("x[:i]: ", x[:i])
             s1 = np.dot(A[i, :i], x[:i])
-            #print("s1: ", s1)
-            #print("A[i, i + 1:]: ", A[i, i + 1:])
-            #print("x[i + 1:]: ", x[i + 1:])
+            # print("s1: ", s1)
+            # print("A[i, i + 1:]: ", A[i, i + 1:])
+            # print("x[i + 1:]: ", x[i + 1:])
             s2 = np.dot(A[i, i + 1:], x[i + 1:])
-            #print("s2: ", s2)
-            #print("x_new",[i]," = " ,b[i]/ A[i, i]," - ",s1/ A[i, i]," - ",s2/ A[i, i])
+            # print("s2: ", s2)
+            # print("x_new",[i]," = " ,b[i]/ A[i, i]," - ",s1/ A[i, i]," - ",s2/ A[i, i])
             x_new[i] = (b[i] - s1 - s2) / A[i, i]
 
         iter = 0
@@ -55,31 +58,32 @@ def Jacobi(A,b,E):
     print(error)
     pass
 
-def Seidel(A,b,E):
+
+def Seidel(A, b, E):
     maxA = 0
-    for i in range(0,A.shape[0]):
-        if A[i,i]>maxA:
-            maxA=A[i,i]
-    x = b/maxA
+    for i in range(0, A.shape[0]):
+        if A[i, i] > maxA:
+            maxA = A[i, i]
+    x = b / maxA
     it_count = 1
     while (True):
-        x_new = b/maxA
-        B =[]
-        for i in range(0,A.shape[0]):
-            #print("A[i, :i]: ", A[i, :i])
-            #print("x_new[:i]: ", x_new[:i])
+        x_new = b / maxA
+        B = []
+        for i in range(0, A.shape[0]):
+            # print("A[i, :i]: ", A[i, :i])
+            # print("x_new[:i]: ", x_new[:i])
             s1 = np.dot(A[i, :i], x_new[:i])
-            #print("s1: ", s1)
-            #print("A[i, i + 1:]: ", A[i, i + 1:])
-            #print("x[i + 1:]: ", x[i + 1:])
+            # print("s1: ", s1)
+            # print("A[i, i + 1:]: ", A[i, i + 1:])
+            # print("x[i + 1:]: ", x[i + 1:])
             s2 = np.dot(A[i, i + 1:], x[i + 1:])
-            #print("s2: ", s2)
-            #print("x_new",[i]," = " ,b[i]/ A[i, i]," - ",s1/ A[i, i]," - ",s2/ A[i, i])
+            # print("s2: ", s2)
+            # print("x_new",[i]," = " ,b[i]/ A[i, i]," - ",s1/ A[i, i]," - ",s2/ A[i, i])
             x_new[i] = (b[i] - s1 - s2) / A[i, i]
-            B.insert(i,[s1 / A[i, i], s2 / A[i, i]])
+            B.insert(i, [s1 / A[i, i], s2 / A[i, i]])
             B[i].insert(i, 0)
 
-        if np.linalg.norm(B,np.inf) > 1:
+        if np.linalg.norm(B, np.inf) > 1:
             print("Норма больше 1!")
             break
 
--- lab13.py
+++ lab13.py
@@ -2,19 +2,19 @@ import numpy as np
 import func
 
 var = 0
-while var not in [1,2]:
+while var not in [1, 2]:
     var = int(input("Выберете метод 1 - Якоби, 2 - Зейделя: "))
 E = -1
 while E < 0:
     E = float(input("Введите погрешность: "))
-A = np.array([[3.1, 0.8, 1.9],[1.9, 3.1, 1.1],[0.5, 3.8, 4.8]])
-#A = np.array([[10, 1, 1],[2, 10, 1],[2, 2, 10]])
+# A = np.array([[3.1, 0.8, 1.9],[1.9, 3.1, 1.1],[0.5, 3.8, 4.8]])
+A = np.array([[10, 1, 1], [2, 10, 1], [2, 2, 10]])
 diag = func.diagonal(A)
 print("Диагональное преобладание: ", diag)
 print()
 
-b = np.array([0.2, 2.1, 5.6])
-#b = np.array([12, 13, 14])
+# b = np.array([0.2, 2.1, 5.6])
+b = np.array([12, 13, 14])
 
 print("Система:")
 for i in range(A.shape[0]):
@@ -22,6 +22,6 @@ for i in range(A.shape[0]):
     print(" + ".join(row), "=", b[i])
 print()
 if var == 1:
-    func.Jacobi(A,b,E)
+    func.Jacobi(A, b, E)
 if var == 2:
-    func.Seidel(A,b,E)
+    func.Seidel(A, b, E)
